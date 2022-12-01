fbf5ad8c6806dc2f2d01d00a657bff1f0f1ec806
Gabriele Mainetti <mg02.gabriele@gmail.com>
2022-12-01 23:47:17 +0100

    hover

--- src/components/films/FilmsCard.vue
+++ src/components/films/FilmsCard.vue
@@ -1,20 +1,26 @@
 <template>
     <div class="card"  >
-        <div class="cardFront" v-on:mouseover="active = !active" v- >
+        <div class="cardFront">
+           <!-- v-on:mouseover="active = !active"-->
             <img class="poster" v-if="singleFilm.poster_path" :src="`https://image.tmdb.org/t/p/w342/${singleFilm.poster_path}`" alt=""> 
             <img class="poster" v-else src="https://th.bing.com/th/id/OIP.joZ4eKstRuAD6HSWmYJrMgHaHa?pid=ImgDet&rs=1" alt="">
         </div> 
-        <div class="cardBack"  v-if="active">
-            <div id="title">{{singleFilm.title}}</div>
-            <div id="originalTitle">{{singleFilm.original_title}}</div>
-            <div id="vote">{{singleFilm.vote_average}}</div> 
-            <div id="language">{{singleFilm.original_language}}</div> 
-            <div id="overview">{{singleFilm.overview}}</div>
+        <div class="cardBack">
+           <!-- v-if="active"-->
+            <div id="title"><span>TITOLO:</span> <h4>{{singleFilm.title}}</h4></div>
+            <div id="originalTitle"><span>TITOLO ORIGINALE:</span> <div>{{singleFilm.original_title}}</div></div>
+            <div id="vote"><span>VOTO:</span> {{singleFilm.vote_average}}</div> 
+            <div class="lingua">
+               <!-- <div id="language"><span>LINGUA:</span> {{singleFilm.original_language}}</div> -->
+               <span>LINGUA:    </span>
+                <img class="flag" :src=" `https://www.countryflagicons.com/FLAT/64/${
+                    (singleFilm.original_language == 'en') ? 'GB' : ((singleFilm.original_language == 'ja') ? 'JP' : singleFilm.original_language.toUpperCase())
+                }.png` " >
+            </div>
+            <div id="overview"><span>TRAMA:</span> <div>{{singleFilm.overview}}</div></div>
             <!--<img :src="`https://image.tmdb.org/t/p/w342/${singleFilm.backdrop_path}`" alt=""> -->
             
-            <img flags :src=" `https://www.countryflagicons.com/FLAT/64/${
-                (singleFilm.original_language == 'en') ? 'GB' : ((singleFilm.original_language == 'ja') ? 'JP' : singleFilm.original_language.toUpperCase())
-            }.png` " >
+            
         </div>
          
     </div>
@@ -32,7 +38,7 @@
 
         data() {
             return {
-                active: false
+               
             }
         },
         methods: {
@@ -52,39 +58,70 @@
 
 .card{
     width: calc(100% / 6);
+    height: 400px;
     background-color: rgb(78, 78, 78);
     border: 4px solid black;
     margin-top: 50px;
-
+    color: white;
     .poster{
         width: 100%;
         height: 100%;
     }
     
     #title{
-        
-        color: white;
         width: 100%;
     }
 
     #originalTitle{
-       
         width: 100%;
+        padding-top: 5px;
     }
 
     #vote{
-        
-        color: white;
         width: 100%;
+        padding-top: 5px;
     }
 
     #language{
-       
-        color: blue;
         width: 100%;
     }
 
- 
+    .lingua{
+        display: flex;
+        margin: auto;
+        align-items: center;
+    }
+
+    .flag{
+        width: 20%;
+    }
+
+    .cardBack{
+        display: none;
+    }
+
+    
+    .card:hover{
+    scale: 1.1;
+    position: relative;
+    z-index: 3;
+    }
+
+    .card:hover .cardFront{
+        display: none;
+    }
+
+    .card:hover .cardBack{
+        display: block;
+        overflow-y: scroll;
+        
+        height: 380px;
+        padding: 10px;
+    }
+
+    span{
+        color: black;
+    }
 
 }
 
--- src/components/series/SeriesCard.vue
+++ src/components/series/SeriesCard.vue
@@ -1,18 +1,25 @@
 <template>
     <div class="card">
-       
-        <img class="poster" v-if="singleSeries.poster_path" :src="`https://image.tmdb.org/t/p/w342/${singleSeries.poster_path}`" alt=""> 
-        <img class="poster" v-else src="https://th.bing.com/th/id/OIP.joZ4eKstRuAD6HSWmYJrMgHaHa?pid=ImgDet&rs=1" alt="">
-        <div id="title">{{singleSeries.name}}</div>
-        <div id="originalTitle">{{singleSeries.original_title}}</div>
-        <div id="vote">{{singleSeries.vote_average}}</div> 
-        <div id="language">{{singleSeries.original_language}}</div> 
-        <!--<img :src="`https://image.tmdb.org/t/p/w342/${singleFilm.backdrop_path}`" alt=""> -->
+       <div class="cardFront">
+            <img class="poster" v-if="singleSeries.poster_path" :src="`https://image.tmdb.org/t/p/w342/${singleSeries.poster_path}`" alt=""> 
+            <img class="poster" v-else src="https://th.bing.com/th/id/OIP.joZ4eKstRuAD6HSWmYJrMgHaHa?pid=ImgDet&rs=1" alt="">
+       </div>
+       <div class="cardBack">
+            <div id="title"><span>TITOLO: </span><h4>{{singleSeries.name}}</h4></div>
+            <div id="originalTitle"><span>TITOLO ORIGINALE: </span><div>{{singleSeries.name}}</div></div>
+            <div id="vote"><span>VOTO: </span> {{singleSeries.vote_average}}</div> 
 
-        <img class="flags" :src=" `https://www.countryflagicons.com/FLAT/64/${
-            (singleSeries.original_language == 'en') ? 'GB' : ((singleSeries.original_language == 'ja') ? 'JP' : singleSeries.original_language.toUpperCase())
-        }.png` " >
-        
+           <!-- <div id="language">{{singleSeries.original_language}}</div> -->
+            <!--<img :src="`https://image.tmdb.org/t/p/w342/${singleFilm.backdrop_path}`" alt=""> -->
+            <div class="lingua">
+                <span>LINGUA:    </span>
+                <img class="flag" :src=" `https://www.countryflagicons.com/FLAT/64/${
+                    (singleSeries.original_language == 'en') ? 'GB' : ((singleSeries.original_language == 'ja') ? 'JP' : singleSeries.original_language.toUpperCase())
+                }.png` " >
+            </div>
+            <div id="overview"><span>TRAMA:</span><div> {{singleSeries.overview}}</div></div>
+
+        </div>
     </div>
 </template>
 
@@ -39,38 +46,69 @@
 
 .card{
     width: calc(100% / 6);
+    height: 400px;
     background-color: rgb(78, 78, 78);
     border: 4px solid black;
     margin-top: 50px;
-
+    color: white;
     .poster{
         width: 100%;
         height: 100%;
     }
 
     #title{
-        color: white;
         width: 100%;
+        
     }
 
     #originalTitle{
         width: 100%;
+        padding-top: 5px;
     }
 
     #vote{
-        color: white;
         width: 100%;
+        padding-top: 5px;
     }
 
     #language{
-        color: blue;
         width: 100%;
     }
 
-   
+    .lingua{
+        display: flex;
+        margin: auto;
+        align-items: center;
+    }
 
-    .flags{
+    .flag{
         width: 20%;
+    }
+
+    .cardBack{
+        display: none;
+    }
+
+    
+    .card:hover{
+    scale: 1.1;
+    position: relative;
+    z-index: 3;
+    }
+
+    .card:hover .cardFront{
+        display: none;
+    }
+
+    .card:hover .cardBack{
+        display: block;
+        overflow-y: scroll;
+        height: 380px;
+        padding: 10px;
+    }
+
+    span{
+        color: black;
     }
 
 }
